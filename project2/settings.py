2b3460411bcdf59e58292280f344b23a777dac66
Yun Sang Woo <Awarduuu@gmail.com>
2020-07-21 17:48:55 +0900

    7/21 project

--- project2/settings.py
+++ project2/settings.py
@@ -122,3 +122,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'project2','media')
